ac7a43e38bc222ac0a0a23e71822f4041d9f6d1b
User <wjdwlsaud_99@naver.com>
2022-09-21 13:04:48 +0900

    이력서 버튼 수정 css 수정 js 간편화

--- asset/js/script.js
+++ asset/js/script.js
@@ -23,21 +23,36 @@ for(let i = 0; i < totalNavList; i++)
     const a = navList[i].querySelector("a");
     a.addEventListener("click", function()
     {
-        for(let i = 0; i < totalSection; i++)
-        {
-            allSection[i].classList.remove("back-section");
-        }
+        removeBackSection();
         for(let j = 0; j < totalNavList; j++)
         {
             if(navList[j].querySelector("a").classList.contains("active"))
             {
-                allSection[j].classList.add("back-section");
+                addBackSection(j);
+                // allSection[j].classList.add("back-section");
             }
             navList[j].querySelector("a").classList.remove("active");
         }
         this.classList.add("active");
         showSection(this);
+        if(window.innerWidth < 1200)
+        {
+            asideSectionTogglerBtn();
+        }
     })
+}
+
+function removeBackSection()
+{
+    for(let i = 0; i < totalSection; i++)
+    {
+        allSection[i].classList.remove("back-section");
+    }
+}
+
+function addBackSection(num)
+{
+    allSection[num].classList.add("back-section");
 }
 
 function showSection(element)
@@ -49,6 +64,27 @@ function showSection(element)
     const target = element.getAttribute("href").split("#")[1];
     document.querySelector("#" + target).classList.add("active");
 }
+
+function updateNav(element)
+{
+    for(let i = 0; i < totalNavList; i++)
+    {
+        navList[i].querySelector("a").classList.remove("active");
+        const target = element.getAttribute("href").split("#")[1];
+        if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1])
+        {
+            navList[i].querySelector("a").classList.add("active");
+        }
+    }
+}
+
+document.querySelector(".hire-me").addEventListener("click", function()
+{
+    const sectionIndex = this.getAttribute("data-section-index");
+
+    showSection(this);
+    updateNav(this);
+})
 
 const navToggleBtn = document.querySelector(".nav-toggler");
 const aside = document.querySelector(".aside");
