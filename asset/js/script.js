99f298ea78661e83b0f37151ee4ff59c7aee7239
User <wjdwlsaud_99@naver.com>
2022-09-21 12:20:50 +0900

    사이드바 토글 반응형 추가

--- asset/js/script.js
+++ asset/js/script.js
@@ -12,10 +12,10 @@ const typed = new Typed(".typing", {
 
 /* ==================== 사이드바 ==================== */
 
-const nav = document.querySelector(".nav");
-const navList = document.querySelectorAll("li");
+const nav = document.querySelector(".nav")
+const navList = document.querySelectorAll("li")
 const totalNavList = navList.length;
-
+const allSection = document.querySelectorAll(".section"), totalSection = allSection.length;
 
 /* ==================== 사이드바 active 활성화/비활성화 ==================== */
 for(let i = 0; i < totalNavList; i++)
@@ -23,12 +23,47 @@ for(let i = 0; i < totalNavList; i++)
     const a = navList[i].querySelector("a");
     a.addEventListener("click", function()
     {
-        for(let j=0; j<totalNavList; j++)
+        for(let i = 0; i < totalSection; i++)
         {
+            allSection[i].classList.remove("back-section");
+        }
+        for(let j = 0; j < totalNavList; j++)
+        {
+            if(navList[j].querySelector("a").classList.contains("active"))
+            {
+                allSection[j].classList.add("back-section");
+            }
             navList[j].querySelector("a").classList.remove("active");
         }
         this.classList.add("active");
+        showSection(this);
     })
 }
 
+function showSection(element)
+{
+    for(let i = 0; i < totalSection; i++)
+    {
+        allSection[i].classList.remove("active");
+    }
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector("#" + target).classList.add("active");
+}
+
+const navToggleBtn = document.querySelector(".nav-toggler");
+const aside = document.querySelector(".aside");
+navToggleBtn.addEventListener("click", () =>
+{
+    asideSectionTogglerBtn();
+})
+
+function asideSectionTogglerBtn()
+{
+    aside.classList.toggle("open");
+    navToggleBtn.classList.toggle("open");
+    for(let i = 0; i < totalSection; i++)
+    {
+        allSection[i].classList.toggle("open");
+    }
+}
 
